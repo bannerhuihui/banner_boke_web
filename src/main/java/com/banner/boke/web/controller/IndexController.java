5f4a27764cedc1527975abb73d84a358352e2feb
yuanhuihui-gwb <yuanhuihui0509@rayootech.com>
2019-02-18 11:17:18 +0800

    半成品提交

---
+++ src/main/java/com/banner/boke/web/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.banner.boke.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView modelAndView){
+        LOGGER.info("进入index方法！");
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
--- src/main/java/com/banner/boke/web/controller/UserController.java
+++ src/main/java/com/banner/boke/web/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.banner.boke.web.controller;
 
 import com.banner.boke.web.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @ResponseBody
     @RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
     public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
-
+        LOGGER.info("进入findAllUser方法！");
         return userService.findAllUser(pageNum,pageSize);
     }
 
